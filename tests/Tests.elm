697d4b32295361f416a0a7adafa5902e5f56f148
kyu08 <kyu08@users.noreply.github.com>
2021-09-19 01:07:25 +0900

    add test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,22 +1,33 @@
 module Tests exposing (..)
 
-import Test exposing (..)
 import Expect
+import Pages.EditLog.Rounds as Rounds
+import Test exposing (..)
+
 
 
 -- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
 
 
-all : Test
-all =
+rounds : Test
+rounds =
     describe "A Test Suite"
-        [ test "Addition" <|
+        [ test "Round4 の順位点を計算する" <|
+            let
+                expectedValue =
+                    Rounds.intRound4ForTesting1Expected
+
+                testValue =
+                    Rounds.calculateRoundFromRawPoint
+                        { round = Rounds.intRound4ForTesting1
+                        , rankPoint = ( 20, 10 )
+                        , havePoint = 25
+                        , returnPoint = 30
+                        }
+            in
             \_ ->
-                Expect.equal 10 (3 + 7)
+                Expect.equal expectedValue testValue
         , test "String.left" <|
             \_ ->
                 Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \_ ->
-                Expect.fail "failed as expected!"
         ]
